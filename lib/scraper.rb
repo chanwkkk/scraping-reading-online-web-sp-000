53e65fbb1a87bfe6f435e66115ef02d6ad400d97
Yingqi Chen <chanwk18@hotmail.com>
2020-04-06 23:20:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+html = open("https://coronavirus.1point3acres.com/en")
+
+doc = Nokogiri::HTML(html)
+
+binding.pry
+
+puts html
